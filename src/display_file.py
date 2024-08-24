63eca2b7d658b2f9c0c49234068bf4293969be24
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-08-24 20:37:57 -0300

    fixing layout

--- src/display_file.py
+++ src/display_file.py
@@ -8,6 +8,7 @@ class DisplayFile:
         self.window = window
         self.viewport = viewport
         self.objects = [Point("First", [(-25, 50)]), Line("Second", [(-40, -32), (10, 90)]), Wireframe("Third", [(-15, 15), (0, 20), (15, 15), (20, 0), (15, -15), (0, -20), (-15, -15), (-20, 0)])]
+        self.draw()
 
     def draw(self):
         self.canvas.delete("all")
--- src/graphics_system.py
+++ src/graphics_system.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 from  window import Window
 from display_file import DisplayFile
-from constants import INITIAL_VIEWPORT, INITIAL_WINDOW
+from constants import VIEWPORT_WIDTH, VIEWPORT_HEIGHT, INITIAL_VIEWPORT, INITIAL_WINDOW
 
 
 class GraphicsSystem:
@@ -14,33 +14,94 @@ class GraphicsSystem:
         self.window = [-100, 100, -100, 100]
 
         self.window = Window()
-        
-        self.canvas = tk.Canvas(self.root, width=800, height=600)
-        self.canvas.grid(column=1, rowspan=7, padx=10, pady=10)
 
-        # TODO: DANDO ERRADO O ZOOM
-        self.zoom_in_button = tk.Button(self.root, text="Zoom In", command=self.zoom_in)
+        self.last_mouse_position = None
+        
+        self.main_frame = tk.Frame(self.root, bg="lightgray")
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.canvas_frame = tk.Frame(self.main_frame)
+        self.canvas_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+        self.canvas = tk.Canvas(self.canvas_frame, width=VIEWPORT_WIDTH, height=VIEWPORT_HEIGHT, bg="white")
+        self.canvas.pack()
+        self.display_file = DisplayFile(self.canvas, self.window, self.viewport)
+
+        self.menu_frame = tk.LabelFrame(self.main_frame, text="Menu", width=200, bg="lightgray", relief="groove", borderwidth=2, font=("Arial", 14, "bold"))
+        self.menu_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
+
+        self.create_objects_list()
+        self.create_nav_buttons()
+        self.create_manipulation_buttons()
+    
+    def create_objects_list(self):
+        self.objects_frame = tk.Frame(self.menu_frame, bg="lightgray", padx=5, pady=5)
+        self.objects_frame.pack(side=tk.TOP)
+
+        self.objects_listbox = tk.Listbox(self.objects_frame)
+        self.objects_listbox.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        self.update_objects_list()
+
+    def update_objects_list(self):
+        self.objects_listbox.delete(0, tk.END)  # Limpa a lista
+        for o in self.display_file.objects:
+            self.objects_listbox.insert(tk.END, o.name)
+
+    def create_nav_buttons(self):
+        self.nav_frame = tk.LabelFrame(self.menu_frame, text="Navigation", width=200, bg="lightgray", relief="groove", borderwidth=2, font=("Arial", 14,))
+        self.nav_frame.pack(side=tk.TOP, padx=5, pady=5)
+
+        self.zoom_buttons = tk.Frame(self.nav_frame, bg="lightgray")
+        self.zoom_buttons.grid(row=0, column=0, padx=10, pady=10)
+
+        self.zoom_in_button = tk.Button(self.zoom_buttons, text="Zoom In", command=self.zoom_in)
         self.zoom_in_button.grid(row=0, column=0, padx=5, pady=5)
 
-        self.zoom_out_button = tk.Button(self.root, text="Zoom Out", command=self.zoom_out)
-        self.zoom_out_button.grid(row=1, column=0, padx=5, pady=5)
+        self.zoom_out_button = tk.Button(self.zoom_buttons, text="Zoom Out", command=self.zoom_out)
+        self.zoom_out_button.grid(row=0, column=1, padx=5, pady=5)
 
-        self.left_button = tk.Button(self.root, text="Esquerda", command=self.move_left)
-        self.left_button.grid(row=3, column=0, padx=5, pady=5)
+        self.directions_buttons = tk.Frame(self.nav_frame, bg="lightgray")
+        self.directions_buttons.grid(row=2, column=0, padx=5, pady=5)
 
-        self.right_button = tk.Button(self.root, text="Direita", command=self.move_right)
-        self.right_button.grid(row=4, column=0, padx=5, pady=5)
+        self.up_button = tk.Button(self.directions_buttons, text="Up", width=7, command=self.move_up)
+        self.up_button.grid(row=0, column=1, padx=5, pady=5)
 
-        self.up_button = tk.Button(self.root, text="Cima", command=self.move_up)
-        self.up_button.grid(row=5, column=0, padx=5, pady=5)
+        self.left_button = tk.Button(self.directions_buttons, text="Left", width=7, command=self.move_left)
+        self.left_button.grid(row=1, column=0, padx=5, pady=5)
 
-        self.down_button = tk.Button(self.root, text="Baixo", command=self.move_down)
-        self.down_button.grid(row=6, column=0, padx=5, pady=5)
-        
-        self.display_file = DisplayFile(self.canvas, self.window, self.viewport)
-        
-        self.display_file.draw()
+        self.right_button = tk.Button(self.directions_buttons, text="Right", width=7, command=self.move_right)
+        self.right_button.grid(row=1, column=2, padx=5, pady=5)
+
+        self.down_button = tk.Button(self.directions_buttons, text="Down", width=7, command=self.move_down)
+        self.down_button.grid(row=2, column=1, padx=5, pady=5)
+
+        self.canvas.bind("<Shift-B1-Motion>", self.move)
+        self.canvas.bind("<ButtonRelease-1>", self.reset_move)
+
+        self.canvas.bind("<MouseWheel>", self.zoom)  # Windows somente?
+        self.canvas.bind("<Button-4>", lambda _: self.zoom_out())  # Linux (scroll up)
+        self.canvas.bind("<Button-5>", lambda _: self.zoom_in())  # Linux (scroll down)
+
+    def create_manipulation_buttons(self):
+        self.manipulation_frame = tk.LabelFrame(self.menu_frame, text="Manipulation", bg="lightgray", relief="groove", borderwidth=2, font=("Arial", 14,))
+        self.manipulation_frame.pack(side=tk.TOP, padx=10, pady=10)
+
+        self.point_create_button = tk.Button(self.manipulation_frame, text="Create Point", command=lambda: None)
+        self.point_create_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.line_create_button = tk.Button(self.manipulation_frame, text="Create Line", command=lambda: None)
+        self.line_create_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.wireframe_create_button = tk.Button(self.manipulation_frame, text="Create Wireframe", command=lambda: None)
+        self.wireframe_create_button.grid(row=0, column=2, padx=5, pady=5)
     
+    def zoom(self, event):
+        if event.delta > 0:
+            self.zoom_in()
+        elif event.delta < 0:
+            self.zoom_out()
+
     def zoom_out(self):
         self.window.zoom_out()
         self.display_file.draw()
@@ -48,6 +109,20 @@ class GraphicsSystem:
     def zoom_in(self):
         self.window.zoom_in()
         self.display_file.draw()
+    
+    def move(self, event):
+        if self.last_mouse_position is None:
+            self.last_mouse_position = (event.x, event.y)
+
+        dx = event.x - self.last_mouse_position[0]
+        dy = event.y - self.last_mouse_position[1]
+        self.last_mouse_position = (event.x, event.y)
+
+        self.window.increase_offset([-dx, dy])
+        self.display_file.draw()
+    
+    def reset_move(self, event):
+        self.last_mouse_position = None
     
     def move_up(self):
         self.window.move_up()
