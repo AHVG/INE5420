40617a7562cdcbb6fbe354df56398857fe1c7115
Breno <brenosilvp@gmail.com>
2024-09-29 22:02:19 -0300

    Sistema de coordenadas normalizado

--- src/model/viewport.py
+++ src/model/viewport.py
@@ -9,13 +9,15 @@ class Viewport:
     def transform(self, drawable):
         points = []
         drawable = drawable.__class__(drawable.name, drawable.points, drawable.color)
-        drawable = Transformation2D(drawable).rotation(np.radians(self.window.angle), self.window.get_offset()).apply()
+        drawable = Transformation2D(drawable).rotation(\
+                   np.radians(self.window.angle), self.window.get_offset()).escalation_relative_to_a_point(\
+                   [1/(self.window.width/2), 1/(self.window.height/2)], self.window.get_offset()).apply()
+
         for point in drawable.points:
             window_bounds = self.window.get_bounds()
             x_viewport = ((point[0] - window_bounds[0]) / (window_bounds[1] - window_bounds[0])) * (self.bounds[1] - self.bounds[0])
             y_viewport = (1 - (point[1] - window_bounds[2]) / (window_bounds[3] - window_bounds[2])) * (self.bounds[3] - self.bounds[2])
-            points.append([x_viewport, y_viewport])
-
+            points.append([x_viewport, y_viewport])        
         return drawable.__class__(drawable.name, points, drawable.color)
 
     def set_window(self, window):
